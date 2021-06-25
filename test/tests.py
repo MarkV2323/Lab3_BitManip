514feb0282a289a2a66ed80c08b193730f2c9ca3
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2021-06-25 12:49:17 -0700

    Setup files for deving

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,5 @@
 # Array of tests to run (in order)
+
 # Each test contains
 #   description - 
 #   steps - A list of steps to perform, each step can have
@@ -11,24 +12,21 @@
 #           If this value is incorrect the test will fail early before completing.
 #       * only one of these should be used
 #   expected - The expected output (as a list of tuples) at the end of this test
+
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+
+# Our Tests (default for now)
+tests = [ 
+    
+    {'description': 'This test will run first.',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 1 } ],
+    'expected': [('PORTB',0x00)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
-    },
+     
     ]
 
-# Optionally you can add a set of "watch" variables these need to be global or static and may need
-# to be scoped at the function level (for static variables) if there are naming conflicts. The 
-# variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['<function>::<static-var>','PORTB']
+# What variables to watch
+watch = ['PORTA']
 
